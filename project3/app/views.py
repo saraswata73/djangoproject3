996c3706208e4a305c67051a6af60d2b50db9114
saraswata73 <168397072+saraswata73@users.noreply.github.com>
2024-05-14 09:46:32 +0530

    Add files via upload

---
+++ project3/app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from app.models import *
+
+# Create your views here.
+def register(request):
+    if request.method=='POST':
+        name=request.POST.get('ename')
+        pno=request.POST.get('pno')
+        email=request.POST.get('email')
+        add=request.POST.get('add')
+        EO=Emp(ename=name,pno=pno,email=email,add=add)
+        EO.save()
+        return HttpResponse('employee registered,successfully')
+    return render(request,'register.html')
+
+
+def home(request):
+    EO=Emp.objects.all()
+    d={'EmployeeObjects':EO}
+    return render(request,'home.html',d)
